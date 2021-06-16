bcd56acb7ff697b5585b8670deee39af71452a79
obent <bentkovska.o@gmail.com>
2021-06-16 22:35:32 +0300

    add database

---
+++ sql.tf
@@ -0,0 +1,64 @@
+module "sql-db_private_service_access" {
+  source      = "GoogleCloudPlatform/sql-db/google//modules/private_service_access"
+  version     = "5.1.1"
+  project_id  = "development-314115"
+  vpc_network = "terraform-network"
+  depends_on = [
+    module.network
+  ]
+}
+
+module "sql-db_mysql" {
+  source  = "GoogleCloudPlatform/sql-db/google//modules/mysql"
+  version = "5.1.1"
+  # insert the 5 required variables here
+  database_version    = "MYSQL_5_7"
+  encryption_key_name = null
+  name                = "wordpress-database1"
+  project_id          = "development-314115"
+  region              = "europe-west1"
+  zone                = "europe-west1-b"
+  db_name             = "wordpress"
+  db_charset          = "utf8"
+  db_collation        = "utf8_unicode_ci"
+  #enable_default_db = false
+  create_timeout = "30m"
+  deletion_protection = false
+
+  user_name     = "wordpressuser"
+  user_password = "qwerty"
+
+  tier = "db-f1-micro"
+  # disk_size = 10
+  # disk_type = "PD_SSD"
+
+ip_configuration = {
+    ipv4_enabled    = false 
+    require_ssl     = null
+    private_network = "projects/development-314115/global/networks/terraform-network"
+    authorized_networks = [
+    #   {
+    #     name  = "private-subnet"
+    #     value = "10.174.0.0/20"
+    #   },
+   ]
+  }
+
+
+  availability_type = "REGIONAL"
+
+ backup_configuration = {
+    enabled                        = true
+    binary_log_enabled             = true
+    start_time                     = "02:55"
+    location                       = null
+    transaction_log_retention_days = 3
+    retained_backups               = 3
+    retention_unit                 = "COUNT"
+  }
+
+
+  module_depends_on = [
+    module.sql-db_private_service_access.peering_completed
+  ]
+}
